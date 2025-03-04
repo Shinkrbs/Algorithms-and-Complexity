43b98e287a3a779ce0cf54cf4cf5a086226122a5
Shn Kerby Dolamos <kerbydolamoscs@gmail.com>
2025-03-05 01:48:19 +0800

    [Laboratory] Added logic for Red-Black Trees

--- Laboratory Exercise Three/add.cpp
+++ Laboratory Exercise Three/add.cpp
@@ -109,7 +109,7 @@ avl_node *insert_avlnode(avl_node *node, int key)
         node->right = insert_avlnode(node->right, key);
     else
         return node;
-        
+
     // Update the balance factor of each node and
     // balance the tree
     node->height = 1 + avl_max(avl_height(node->left), avl_height(node->right));
@@ -137,4 +137,178 @@ avl_node *insert_avlnode(avl_node *node, int key)
         }
     }
     return node;
+}
+
+// Red-Black Tree
+struct rbt_node 
+{
+    int data;
+    rbt_node *parent;
+    rbt_node *left;
+    rbt_node *right;
+    int color;
+};
+
+typedef rbt_node *NodePtr;
+
+class RedBlackTree 
+{
+    private:
+        NodePtr root;
+        NodePtr TNULL;
+    
+    public:
+    void initializeNULLNode(NodePtr node, NodePtr parent) 
+    {
+        node->data = 0;
+        node->parent = parent;
+        node->left = nullptr;
+        node->right = nullptr;
+        node->color = 0;
+    }
+
+    // Rotation left
+    void leftRotate(NodePtr x) 
+    {
+        NodePtr y = x->right;
+        x->right = y->left;
+
+        if (y->left != TNULL) 
+            y->left->parent = x;
+
+        y->parent = x->parent;
+        if (x->parent == nullptr) 
+            this->root = y;
+        else if (x == x->parent->left) 
+            x->parent->left = y;
+        else 
+            x->parent->right = y;
+        
+        y->left = x;
+        x->parent = y;
+    }
+
+    // Rotation right
+    void rightRotate(NodePtr x) 
+    {
+        NodePtr y = x->left;
+        x->left = y->right;
+        if (y->right != TNULL) 
+            y->right->parent = x;
+
+        y->parent = x->parent;
+        if (x->parent == nullptr) 
+            this->root = y;
+        else if (x == x->parent->right) 
+            x->parent->right = y;
+        else 
+            x->parent->left = y;
+        
+        y->right = x;
+        x->parent = y;
+    }
+
+    // For balancing the tree after insertion
+    void insertFix(NodePtr k) 
+    {
+        NodePtr u;
+        while (k->parent->color == 1) 
+        {
+            if (k->parent == k->parent->parent->right) 
+            {
+                u = k->parent->parent->left;
+                if (u->color == 1) 
+                {
+                    u->color = 0;
+                    k->parent->color = 0;
+                    k->parent->parent->color = 1;
+                    k = k->parent->parent;
+                } 
+                else 
+                {
+                    if (k == k->parent->left) 
+                    {
+                        k = k->parent;
+                        rightRotate(k);
+                    }
+                    k->parent->color = 0;
+                    k->parent->parent->color = 1;
+                    leftRotate(k->parent->parent);
+                }
+            } 
+            else 
+            {
+                u = k->parent->parent->right;
+
+                if (u->color == 1) 
+                {
+                    u->color = 0;
+                    k->parent->color = 0;
+                    k->parent->parent->color = 1;
+                    k = k->parent->parent;
+                } 
+                else 
+                {
+                    if (k == k->parent->right) 
+                    {
+                        k = k->parent;
+                        leftRotate(k);
+                    }
+                    k->parent->color = 0;
+                    k->parent->parent->color = 1;
+                    rightRotate(k->parent->parent);
+                }
+            }
+            if (k == root) 
+                break;
+        }
+        root->color = 0;
+    }
+
+    // Inserting a node
+    void insert(int key) 
+    {
+        NodePtr node = new rbt_node;
+        node->parent = nullptr;
+        node->data = key;
+        node->left = TNULL;
+        node->right = TNULL;
+        node->color = 1;
+
+        NodePtr y = nullptr;
+        NodePtr x = this->root;
+
+        while (x != TNULL) 
+        {
+            y = x;
+            if (node->data < x->data) 
+                x = x->left;
+            else 
+                x = x->right;
+        }
+
+        node->parent = y;
+        if (y == nullptr) 
+            root = node;
+        else if (node->data < y->data) 
+            y->left = node;
+        else
+            y->right = node;
+
+        if (node->parent == nullptr) 
+        {
+            node->color = 0;
+            return;
+        }
+
+        if (node->parent->parent == nullptr) 
+            return;
+
+        insertFix(node);
+    }
+};
+
+int main()
+{
+    return 0;
 }
